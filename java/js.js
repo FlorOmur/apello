3c98320331f62ab35856d0b9542bb06446ea4864
Flor Omurbekov <flor.omurbekov@mail.ru>
2022-01-17 16:23:16 +0300

    js-files

--- java/js.js
+++ java/js.js
@@ -6,9 +6,27 @@ myBurger.addEventListener('click', () => {
     myMinu.classList.toggle('show')
 })
 
-menuLinks.forEach(onelink =>{
-    onelink.addEventListener('click', () =>{
+menuLinks.forEach(onelink => {
+    onelink.addEventListener('click', () => {
         myMinu.classList.remove('show')
     })
 })
 
+AOS.init();
+
+$(document).ready(function () {
+    $('.expand-collapse h3').each(function () {
+        var tis = $(this), state = false, answer = tis.next('div').slideUp();
+        tis.click(function () {
+            state = !state;
+            answer.slideToggle(state);
+            tis.toggleClass('active', state);
+        });
+    });
+});
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+    });
+});
